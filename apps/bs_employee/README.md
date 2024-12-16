1e39b7d8cb51bcd8514b2134bef4fde5db3fb619
seeprogramming <sagarshelke18@gmail.com>
2024-12-16 20:02:47 +0530

    First commit in bs_employee

---
+++ apps/bs_employee/README.md
@@ -0,0 +1,1 @@
+# BankSphere - Employee Portal
---
+++ apps/bs_employee/src/App.jsx
@@ -0,0 +1,6 @@
+import { Header } from '@banksphere/ui_library';
+function App() {
+    return <Header profile_name='SaDa' application_name='BankSphere Employee'></Header>;
+}
+
+export default App;
