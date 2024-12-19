48a5b60565f7619a5966f7074d99b26f9eaec271
seeprogramming <sagarshelke18@gmail.com>
2024-12-19 12:46:14 +0530

    Add validators

--- apps/bs_backend/routes/auth.routes.js
+++ apps/bs_backend/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { register, login, logout } = require('../controllers/auth.controller');
+const validateRequest = require('../utils/validateRequest');
+const { loginValidation, registerValidation } = require('../utils/validatators/authValidator');
 
 const router = express.Router();
 
-router.post('/register', register); // DONE
-router.post('/login', login); // DONE
+router.post('/register', validateRequest(registerValidation), register);
+router.post('/login', validateRequest(loginValidation), login);
 router.post('/logout', logout); //
 
 module.exports = router;
