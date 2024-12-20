7814771a3102aa4414770eaaf4967ca4d8f48e7c
seeprogramming <sagarshelke18@gmail.com>
2024-12-20 17:10:30 +0530

    Add rate limiter for login and registration

--- apps/bs_backend/routes/auth.routes.js
+++ apps/bs_backend/routes/auth.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { register, login, logout } = require('../controllers/auth.controller');
-const validateRequest = require('../utils/validateRequest');
-const { loginValidation, registerValidation } = require('../utils/validatators/authValidator');
+const validateRequest = require('../utils/validateRequestHandler');
+const { loginValidation, registerValidation } = require('../utils/validators/authValidator');
+const limiter = require('../utils/rateLimitHandler');
 
 const router = express.Router();
 
-router.post('/register', validateRequest(registerValidation), register);
-router.post('/login', validateRequest(loginValidation), login);
+router.post('/register', limiter, validateRequest(registerValidation), register);
+router.post('/login', limiter, validateRequest(loginValidation), login);
 router.post('/logout', logout); //
 
 module.exports = router;
